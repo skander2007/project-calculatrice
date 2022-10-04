146ef877bd4fd2c8752cab9898b4f357f10d8183
skander2007 <skanderjallouligmc@gmail.com>
2022-10-04 16:37:47 +0100

    first commit

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,11 +1,11 @@
-import {ACTIONS } from './App'
+import {ACTIONS } from "./App";
 
 export default function DigitButton({ dispatch, digit}) {
     return (
      <button 
-      onClick={() => dispatch({ type:ACTIONS.ADD_DIGIT,payload: { digit }})}
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT,payload: { digit } })}
     >
         {digit}
         </button>
-    )   
+    );   
 }
--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,10 +1,12 @@
-import { ACTIONS } from "./App"
+import { ACTIONS } from "./App";
 export default function OperationButton ({ dispatch,operation}) {
     return (
         <button
-        onClick={() => dispatch ({ type: ACTIONS.CHOOSE_OPERATION,payload: { operation}}) }
+        onClick={() => 
+            dispatch ({ type: ACTIONS.CHOOSE_OPERATION,payload: { operation } })
+         }
         >
             {operation}
         </button>
-    )
+    );
 }
